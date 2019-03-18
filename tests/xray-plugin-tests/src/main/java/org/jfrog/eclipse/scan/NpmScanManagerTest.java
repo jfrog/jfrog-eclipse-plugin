1b088b40cd7f053670b6025ca45a27629b36183d
yahavi <yahavi@jfrog.com>
2019-03-18 18:14:04 +0200

    Add tests infrastructure

---
+++ tests/xray-plugin-tests/src/main/java/org/jfrog/eclipse/scan/NpmScanManagerTest.java
@@ -0,0 +1,13 @@
+package org.jfrog.eclipse.scan;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class NpmScanManagerTest {
+    
+	@Test
+    public void testPass() {
+        assertTrue(true);
+    }
+}
