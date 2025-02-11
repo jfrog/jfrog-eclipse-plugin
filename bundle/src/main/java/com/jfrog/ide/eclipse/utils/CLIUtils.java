22ba670e9ad5059de3c04bb1041773d4bd39bc68
kerenr-jfrog <kerenr@jfrog.com>
2025-02-11 17:38:24 +0200

    progress

--- bundle/src/main/java/com/jfrog/ide/eclipse/utils/CLIUtils.java
+++ bundle/src/main/java/com/jfrog/ide/eclipse/utils/CLIUtils.java
@@ -3,22 +3,23 @@ package com.jfrog.ide.eclipse.utils;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.io.IOException;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.jfrog.ide.eclipse.log.Logger;
 
 public class CLIUtils {
 	
 	public static final String JFROG_CLI_RELEASES_URL = "https://releases.jfrog.io/artifactory/jfrog-cli/v2-jf/";
-	public static final String JFROG_CLI_VERSION = "2.73.4";
+	public static final String JFROG_CLI_FIXED_VERSION = "2.73.3";
 	
 	public static Logger logger = Logger.getInstance();
 	
-	public static void downloadCLIIfNeeded() {
+	public static void downloadCLIIfNeeded() throws IOException {
 		String systemFolderPath;
-		// get OS type and architecture
+		// get OS type 
 		String osName = System.getProperty("os.name").toLowerCase();
-		String osVersion = System.getProperty("os.version");
-		String osArch = System.getProperty("os.arch");
 		
         if (osName.contains("win")) {
             // For Windows, use SystemRoot environment variable
@@ -38,27 +39,61 @@ public class CLIUtils {
         // check if jfrog.exe file exist in system folder. if not download it
         if (!Files.exists(jfrogExeFilePath)) {
         	logger.info("'jf.exe' file is not cached locally. Downloading it now...");
-        	downloadCliFromReleases(osName);
+        	downloadCliFromReleases(osName, JFROG_CLI_FIXED_VERSION);
         } else {
         	logger.info("'jf.exe' file is cached locally. File location: " + jfrogExeFilePath);
         	// run jf.exe --version in a process to verify correct CLI version
         }
 	}
 	
-	public static String getCLIPath() {
+	
+	public static void downloadCliFromReleases(String osName, String cliVersion) throws IOException {
+		String osAndArch = getOSAndArc();
+		String fullCLIPath = JFROG_CLI_RELEASES_URL + cliVersion + "/jfrog-cli-" + osAndArch + "/jf.exe";
+		String destinationPath = ""; // where we want to save the file? 
 		
-		return "";
+		// download jf.exe from 'fullCLIPath' and save it at 'destinationPath'
+		
 	}
 	
-	
-	public static void downloadCliFromReleases(String osName) {
-		String cliVersion = "";
-
-		// download the relevant jf.exe version according to the defined version & OS type.
-		if (osName.contains("win")) {
-			logger.info("Downloading CLI for windows");
-		} else {
-			logger.info("Downloading CLI for linux/macOS");
-		}
-	}
+    public static String getOSAndArc() throws IOException {
+        String arch = SystemUtils.OS_ARCH;
+        // Windows
+        if (SystemUtils.IS_OS_WINDOWS) {
+            return "windows-amd64";
+        }
+        // Mac
+        if (SystemUtils.IS_OS_MAC) {
+            if (StringUtils.equalsAny(arch, "aarch64", "arm64")) {
+                return "mac-arm64";
+            } else {
+                return "mac-amd64";
+            }
+        }
+        // Linux
+        if (SystemUtils.IS_OS_LINUX) {
+            switch (arch) {
+                case "i386":
+                case "i486":
+                case "i586":
+                case "i686":
+                case "i786":
+                case "x86":
+                    return "linux-386";
+                case "amd64":
+                case "x86_64":
+                case "x64":
+                    return "linux-amd64";
+                case "arm":
+                case "armv7l":
+                    return "linux-arm";
+                case "aarch64":
+                    return "linux-arm64";
+                case "ppc64":
+                case "ppc64le":
+                    return "linux-" + arch;
+            }
+        }
+        throw new IOException(String.format("Unsupported OS: %s-%s", SystemUtils.OS_NAME, arch));
+    }
 }
