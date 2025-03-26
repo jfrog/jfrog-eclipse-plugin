4ee7be334e61bc7871e4922ba52278e3ecd2f846
Eyal Kapon <eyalk@jfrog.com>
2025-03-26 11:27:18 +0200

    fix up bugs

--- bundle/src/main/java/com/jfrog/ide/eclipse/configuration/CliDriverWrapper.java
+++ bundle/src/main/java/com/jfrog/ide/eclipse/configuration/CliDriverWrapper.java
@@ -5,41 +5,36 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-
 
 import com.jfrog.ide.common.configuration.JfrogCliDriver;
 import com.jfrog.ide.eclipse.log.Logger;
 
 public class CliDriverWrapper {
 
-    // Private static instance of the class (Singleton)
     private static CliDriverWrapper instance;
 
-    // Instance of JfrogCliDriver
-    private JfrogCliDriver cliDriver;
-
-    // Constants
     public static final String CLIENT_ID_SERVER = "eclipse";
     public static final String CLI_VERSION = "2.74.1";
-	public static final Path HOME_PATH = Paths.get(System.getProperty("user.home"), ".jfrog-eclipse-plugin");
+    public static final Path HOME_PATH = Paths.get(System.getProperty("user.home"), ".jfrog-eclipse-plugin");
 
+    private JfrogCliDriver cliDriver;
 
-    // Private constructor to prevent instantiation from outside
     private CliDriverWrapper() {
         try {
             Files.createDirectories(HOME_PATH);
-        } catch (IOException e) {
-            this.showCliError(e);
+        } catch (Exception e) {
+            showCliError(e);
         }
         // Initialize the cliDriver and download CLI if needed
         this.cliDriver = new JfrogCliDriver(null, Logger.getInstance());
         try {
             this.cliDriver.downloadCliIfNeeded(HOME_PATH.toString(), CLI_VERSION);
         } catch (IOException e) {
-            this.showCliError(e);
+            showCliError(e);
         }
     }
 
@@ -58,15 +53,16 @@ public class CliDriverWrapper {
         return cliDriver;
     }
 
-    private void showCliError(Error e){
-        Logger.getInstance().error(e.getMessage());
+    private void showCliError(Exception e) {
+        Logger.getInstance().error(e.getMessage(), e);
         IStatus status = new Status(IStatus.ERROR, "jfrog-eclipse-plugin", "An error occurred: " + e.getMessage(), e);
-            Display.getDefault().asyncExec(() -> {
-                Shell shell = Display.getDefault().getActiveShell();
-                if (shell != null) {
-                    ErrorDialog.openError(shell, "Error", "An error occurred", status);
-                }
-            });
-        }
-}
 
+        // Run UI-related code on the main UI thread
+        Display.getDefault().asyncExec(() -> {
+            Shell shell = Display.getDefault().getActiveShell();
+            if (shell != null) {
+                ErrorDialog.openError(shell, "Error", "An error occurred", status);
+            }
+        });
+    }
+}
