9e6fccf185ddc76601ac566f486ad698433f9e49
kerenr-jfrog <kerenr@jfrog.com>
2025-05-13 18:25:18 +0300

    added integration test

--- tests/src/main/java/com/jfrog/ide/eclipse/scan/ScanManagerTest.java
+++ tests/src/main/java/com/jfrog/ide/eclipse/scan/ScanManagerTest.java
@@ -2,6 +2,7 @@ package com.jfrog.ide.eclipse.scan;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,6 +17,7 @@ import org.eclipse.swt.widgets.Composite;
 
 import com.jfrog.ide.common.nodes.FileTreeNode;
 import com.jfrog.ide.eclipse.scheduling.CliJob;
+import com.jfrog.ide.eclipse.ui.issues.IssuesTree;
 import com.jfrog.ide.eclipse.utils.Utils;
 
 import junit.framework.TestCase;
@@ -23,28 +25,41 @@ import junit.framework.TestCase;
 public class ScanManagerTest extends TestCase {
 	private ScanManager scanManager = ScanManager.getInstance();
 	private ScanCache scanCache = ScanCache.getInstance();
+	private IssuesTree issuesTree = IssuesTree.getInstance();
 
 	
 	// TODO: generate test for scanning: Maven, Gradle and NPM projects. 
 	// TODO: add resources dir with example projects to scan
-	public void testScanMavenProject() throws IOException, CoreException {
+	public void testScanMavenProject() throws IOException, CoreException, InterruptedException {
 		String projectName = "mavenIsApplicable";
-		JobListener jobListener = new JobListener();
+		CountDownLatch latch = new CountDownLatch(1);
+		
+		JobListener jobListener = new JobListener(latch);
 		IProject project = Utils.createProject(projectName, "maven");
+		
 		Job.getJobManager().addJobChangeListener(jobListener);
 		
-		scanManager.startScan(null, false);
-		List<FileTreeNode> scanResults = scanCache.getScanResults();
+		scanManager.scanAndUpdateResults(issuesTree, new Composite(null, 0), project, null);
+		
+		// wait for scanAndUpdateResults to return, then check issuesTree has results 
+		 boolean completed = latch.await(60, java.util.concurrent.TimeUnit.SECONDS);
+		
+        // Ensure the job completed
+        assertTrue(completed);
+
+        // Check the issuesTree has results
+        assertNotNull(issuesTree.getChildren());
+        assertTrue(issuesTree.getChildren().length > 0);
 		
 	}
 	
 	public void testSchedulingAJob()
 			throws IOException, CoreException, OperationCanceledException, InterruptedException {
 		String projectName = "gradleIsApplicable";
-		JobListener jobListener = new JobListener();
+		JobListener jobListener = new JobListener(null);
 		IProject project = Utils.createProject(projectName, "gradle");
 		Job.getJobManager().addJobChangeListener(jobListener);
-		scanManager.scanAndUpdateResults(null, new Composite(null, 0), null, null);
+		scanManager.scanAndUpdateResults(null, null, project, null);
 		Job.getJobManager().join(CliJob.FAMILY, new NullProgressMonitor());
 		assertJobInformation(projectName, jobListener);
 		cleanup(jobListener);
@@ -53,10 +68,10 @@ public class ScanManagerTest extends TestCase {
 	public void testScanFinished() throws IOException, CoreException, OperationCanceledException, InterruptedException {
 		scanManager.getScanInProgress().set(true);
 		String projectName = "gradleIsApplicable";
-		JobListener jobListener = new JobListener();
+		JobListener jobListener = new JobListener(null);
 		Job.getJobManager().addJobChangeListener(jobListener);
 		IProject project = Utils.createProject(projectName, "gradle");
-		scanManager.scanAndUpdateResults(null, null, null, null);
+		scanManager.scanAndUpdateResults(null, null, project, null);
 		Job.getJobManager().join(CliJob.FAMILY, new NullProgressMonitor());
 		assertJobInformation(projectName, jobListener);
 		assertFalse(scanManager.getScanInProgress().get());
@@ -71,7 +86,7 @@ public class ScanManagerTest extends TestCase {
 
 	private void assertJobInformation(String projectName, JobListener jobListener) {
 		assertTrue(jobListener.isJobExists());
-		assertEquals(projectName, jobListener.getJobName());
+		assertEquals("Performing Scan: " + projectName, jobListener.getJobName());
 		assertEquals(1, jobListener.numOfJobs());
 	}
 
@@ -80,7 +95,12 @@ public class ScanManagerTest extends TestCase {
 		private static AtomicBoolean jobExists = new AtomicBoolean(false);
 		private String jobName;
 		private static AtomicInteger numOfJobs = new AtomicInteger();
+		private final CountDownLatch latch;
 
+        public JobListener(CountDownLatch latch) {
+            this.latch = latch;
+        }
+		
 		@Override
 		public void scheduled(IJobChangeEvent event) {
 			if (event.getJob().belongsTo(CliJob.FAMILY)) {
@@ -93,6 +113,9 @@ public class ScanManagerTest extends TestCase {
 		public void done(IJobChangeEvent event) {
 			if (event.getJob().belongsTo(CliJob.FAMILY)) {
 				jobExists.set(true);
+			}
+			if (latch != null) {
+				latch.countDown();
 			}
 		}
 
